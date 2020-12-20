da499c892ee176fa10b7cb69963c1ca52cb66e80
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2020-12-20 13:34:46 +0900

    Implement the first version

---
+++ generate-note.py
@@ -0,0 +1,21 @@
+from collections import defaultdict
+import csv
+from io import TextIOWrapper
+from urllib.request import urlopen
+
+holidays = defaultdict(list)
+with urlopen('https://www8.cao.go.jp/chosei/shukujitsu/syukujitsu.csv') as f:
+    if f.status == 200:
+        for row in csv.DictReader(TextIOWrapper(f, encoding='cp932', newline='')):
+            holidays[row['国民の祝日・休日名称']].append(row['国民の祝日・休日月日'])
+    else:
+        raise 'Something was wrong'
+
+print('---')
+print('events:')
+for name, days in holidays.items():
+    times = [f'start: {d.replace("/", "-")}' for d in days]
+    print(f'  "{name}":')
+    print(f'    color: red')
+    print(f'    times: [{", ".join(times)}]')
+print('---')
